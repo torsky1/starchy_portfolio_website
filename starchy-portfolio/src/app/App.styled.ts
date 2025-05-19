3194e30548f3b5ed710a2dde58edf8811753ae6a
ziarenn <epziarnisty@gmail.com>
2025-05-19 15:10:21 +0200

    task3/ Set up external libraries

---
+++ starchy-portfolio/src/app/App.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`;
+
+export const AppHeader = styled.h1``;
---
+++ starchy-portfolio/src/app/App.tsx
@@ -0,0 +1,10 @@
+import * as Styled from "./App.styled";
+function App() {
+  return (
+    <>
+      <Styled.AppHeader>Starchy Portfolio</Styled.AppHeader>
+    </>
+  );
+}
+
+export default App;
