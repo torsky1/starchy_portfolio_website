424cb3367bf124e52b2468a359af4691c0595e6e
torsky1 <wiktortwardowski1212@gmail.com>
2025-09-05 13:19:07 +0200

    deploy changes again

--- starchy-portfolio/vite.config.ts
+++ starchy-portfolio/vite.config.ts
@@ -3,11 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/starchy-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./public/assets"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
       "@components": path.resolve(__dirname, "./src/components"),
     },
   },
