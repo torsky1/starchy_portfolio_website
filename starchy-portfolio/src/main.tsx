4ece3fac9fdefae2bf853fb764b4d9ea895ed170
torsky1 <wiktortwardowski1212@gmail.com>
2025-09-09 15:00:38 +0200

    refactor: update ESLint configuration and fix import paths in main.tsx

--- starchy-portfolio/src/main.tsx
+++ starchy-portfolio/src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./app/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>
--- starchy-portfolio/vite.config.ts
+++ starchy-portfolio/vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/starchy_portfolio_website/",
+  build: { outDir: "dist", emptyOutDir: true },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
